9128dd7a92b4b1f1df51dfe8bf9dc490edb8f5e8
Hyo789 <minorumineta789@gmail.com>
2022-05-29 00:44:41 +0200

    MAJ fonctions Page About-Me (JS) Laptop

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,17 +37,67 @@ let switchSection = function (section) {
         navSecondary.classList.replace(navSecondaryClass, section);
     }
 
-}
+};
+
+// ACCUEIL
+
+(function () {
+
+    const cards_a1 = document.querySelector(".cards.a1");
+    cards_a1.innerHTML = '<div class="container"></div>';
+
+    const container = cards_a1.querySelector(".container");
+    container.innerHTML = '<article class="accueil_article"></article> <div class="accueil_article_illustration"></div>';
+
+    const article = container.querySelector(".accueil_article");
+    article.innerHTML = '<div class="accueil_article_profil"></div> <p></p>';
+
+    const profil = article.querySelector(".accueil_article_profil");
+    profil.innerHTML = '<img src="" alt="Image de profil"> <h2></h2>';
+    profil.querySelector("img").src = './assets/img/logo/logo.svg';
+    profil.querySelector("h2").innerHTML = "faizdine";
+
+    article.querySelector("p").textContent = 
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dicta enim repellendus tempora magni quod consectetur magnam fuga repellat necessitatibus ab aliquam";
+
+    const illustration = container.querySelector(".accueil_article_illustration");
+    illustration.innerHTML = '<img src="" alt="Illustration de l\'article">';
+    illustration.querySelector('img').src = './assets/img/Fichier 1.svg';
+    
+    
+    const cards_a2 = document.querySelector(".cards.a2");
+    cards_a2.innerHTML = '<img src="" alt="Illustration de la card">'
+    cards_a2.querySelector('img').src = './assets/img/Fichier 1.svg';
+    
+    const cards_a3 = document.querySelector(".cards.a3");
+    cards_a3.innerHTML = '<img src="" alt="Illustration de la card">'
+    cards_a3.querySelector('img').src = './assets/img/Fichier 1.svg';
+    
+    const cards_a4 = document.querySelector(".cards.a4");
+    cards_a4.innerHTML = '<img src="" alt="Illustration de la card">'
+    cards_a4.querySelector('img').src = './assets/img/Fichier 1.svg';
+}) ();
+
+// ABOUT ME
 
 (function () {
 
     let contentAbout_profil = function () {
-        let element = document.querySelector(".cards.a1");
-        element.innerHTML= '<div></div>';
-        element.firstElementChild.innerHTML='</div><div><img src="" alt=""> </div> <div> <h3></h3> <p></p> </div>'
+
+        let cards_a1 = document.querySelector(".cards.a1");
+        cards_a1.innerHTML= '<div class="container"></div>';
+
+        const container = cards_a1.querySelector(".container");
+        container.innerHTML='<div class="profil_illustration"><img src="" alt="Illustration"> </div> <article> <h3></h3> <p></p> </article>';
     
-        element.querySelector("h3").innerHTML = "C'est ok pour le titre"
-        element.querySelector("p").innerHTML = "C'est ok pour faire le boulot"
+        container.querySelector("img").src = './assets/img/Fichier 1.svg';
+        container.querySelector("h3").innerHTML = "Présentation"
+        container.querySelector("p").textContent = 
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut dicta enim repellendus tempora magni quod consectetur magnam fuga repellat necessitatibus ab aliquam"
+    }
+
+    let contentAbout_competences = function () {
+        
     }
 
     nav_About.addEventListener("click", () => {
@@ -62,17 +112,20 @@ let switchSection = function (section) {
             }else {
                 continue
             }
-            console.log(el);
         }
+        contentAbout_profil();
+        about_profil.classList.add("active");
     })
 
     about_profil.addEventListener("click", () => {
         contentAbout_profil();
     });
 
-    about_competences.addEventListener();
-    about_formations.addEventListener();
-    about_interet.addEventListener();
+    about_competences.addEventListener("click", () => {
+
+    });
+    // about_formations.addEventListener();
+    // about_interet.addEventListener();
 
 }) ();
 
@@ -89,22 +142,22 @@ nav_Creations.addEventListener("click", () => {
         }
         console.log(el);
     }
-})
+});
 
 nav_Travaux.addEventListener("click", () => {
     let sectionClass = nav_Travaux.getAttribute("id");
     switchSection(sectionClass)
-})
+});
 
 nav_ressources.addEventListener("click", () => {
     let sectionClass = nav_ressources.getAttribute("id");
     switchSection(sectionClass)
-})
+});
 
 nav_bonus.addEventListener("click", () => {
     let sectionClass = nav_bonus.getAttribute("id");
     switchSection(sectionClass)
-})
+});
 
 
 
